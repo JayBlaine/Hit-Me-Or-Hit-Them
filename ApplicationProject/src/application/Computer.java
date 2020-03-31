0ee4e25322033b32d16c3f7c046822674086a780
JayBlaine <jbrags42@gmail.com>
2020-03-31 17:35:37 -0500

    Added more getters/setters, organized code into more specialized functions

--- ApplicationProject/src/application/Computer.java
+++ ApplicationProject/src/application/Computer.java
@@ -5,14 +5,17 @@ import java.util.ArrayList;
 public class Computer {
 	
 	private Card deck;
+	private double responseTime;
+	private int diff;
+	double implementChaos = Math.random();
 	
-	public Computer(int diff) {
+	public Computer(int iDiff) {
 		//1 for easy, 2 for medium, 3 for hard
-		
+		diff = iDiff;
 		deck = new Card();
-		double implementChaos = Math.random();
-		double responseTime;
 		
+	}
+	public void setDifficulty() {
 		switch (diff) {
 			case 1:
 				responseTime = implementChaos + 1;
@@ -20,12 +23,16 @@ public class Computer {
 				responseTime = (implementChaos * 0.5) + 0.75;
 			case 3:
 				responseTime = (implementChaos * 0.33) + 0.5;
-		}
+			}
 	}
-	
-	public ArrayList getComputerDeck() {
-		return deck.getDeck();
-	}
+	public double getResponseTime() {
+		return responseTime;}
+	public void setResponseTime(double newTime) {
+		responseTime = newTime;}
+	public Card getComputerDeck() {
+		return deck;}
+	public void setDeck(Card newDeck) {
+		deck = newDeck;}
 	
 
 }
