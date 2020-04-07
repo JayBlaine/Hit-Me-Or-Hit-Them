a84233927232f711bc7248fdf724263c2367c5bb
tiffany <61089246+mangosteenmilk@users.noreply.github.com>
2020-04-07 17:40:50 -0500

    Add files via upload
    Documentation and change to GUI - fixed unevenness and centered everything

--- ApplicationProject/src/application/Computer.java
+++ ApplicationProject/src/application/Computer.java
@@ -1,43 +1,33 @@
 package application;
 
-//import java.util.ArrayList;
 
 public class Computer {
 	
-	//private ArrayList<String> deck = Card.getDeck();
 	private double responseTime;
 	private int diff;
 	private double implementChaos = Math.random();
 	
 	public Computer(int iDiff) {
-		//1 for easy, 2 for medium, 3 for hard
 		diff = iDiff;
-		//deck = new Card();
 		
 	}
 	public void setDifficulty(int newDiff) {
 		switch (newDiff) {
 			case 1:
-				responseTime = implementChaos + 1;
+				responseTime = implementChaos + 1;						//set response time to random number (slow)
 			case 2:
-				responseTime = (implementChaos * 0.5) + 0.75;
+				responseTime = (implementChaos * 0.5) + 0.75;			//set response time to random number(decent)
 			case 3:
-				responseTime = (implementChaos * 0.33) + 0.5;
+				responseTime = (implementChaos * 0.33) + 0.5;			//set response time to random number (fast)
 			}
-		//No need for default, only 1,2,3 are possible
+																		//No need for default, only 1,2,3 are possible
 	}
-	public double getDifficulty() {
-		return diff;}
-	public double getResponseTime() {
-		return responseTime;}
-	public void setResponseTime(double newTime) {
-		responseTime = newTime;}
-	/*
-	public Card getComputerDeck() {
-		return deck;}
-	public void setDeck(Card newDeck) {
-		deck = newDeck;}
-		*/
+	public double getDifficulty() { 	return diff;}					//accessors
+	
+	public double getResponseTime() {	return responseTime;}			//accessors
+	
+	public void setResponseTime(double newTime) {	responseTime = newTime;}//mutators
+	
 	
 
 }
