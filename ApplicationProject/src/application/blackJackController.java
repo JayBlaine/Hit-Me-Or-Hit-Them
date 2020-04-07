34ca5d1cd6d0db2a9c19c4d2692f171baca3d961
JayBlaine <jbrags42@gmail.com>
2020-04-06 19:50:08 -0500

    Removed old blackjack graphics

--- ApplicationProject/src/application/blackJackController.java
+++ ApplicationProject/src/application/blackJackController.java
@@ -22,46 +22,6 @@ import javafx.scene.shape.Rectangle;
 public class blackJackController 
 {
 	@FXML
-	private Rectangle p1;
-	@FXML
-	private Rectangle p2;
-	@FXML
-	private Rectangle p3;
-	@FXML
-	private Rectangle p4;
-	@FXML
-	private Rectangle p5;
-	@FXML
-	private Rectangle d1;
-	@FXML
-	private Rectangle d2;
-	@FXML
-	private Rectangle d3;
-	@FXML
-	private Rectangle d4;
-	@FXML
-	private Rectangle d5;
-	@FXML
-	private Label lblp1;
-	@FXML
-	private Label lblp2;
-	@FXML
-	private Label lblp3;
-	@FXML
-	private Label lblp4;
-	@FXML
-	private Label lblp5;
-	@FXML
-	private Label lbld1;
-	@FXML
-	private Label lbld2;
-	@FXML
-	private Label lbld3;
-	@FXML
-	private Label lbld4;
-	@FXML
-	private Label lbld5;
-	@FXML
 	private Label lblDeck;
 	@FXML
 	private Label txtHelp;
